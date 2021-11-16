0422be9f2c8d299690692d375b242dd261cd30fa
modesty0310 <rlatprua4@gmail.com>
2021-11-16 20:43:59 +0900

    password hash

--- admin/settings.py
+++ admin/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+AUTH_USER_MODEL = 'users.User'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- users/serializers.py
+++ users/serializers.py
@@ -8,3 +8,11 @@ class UserSerializer(serializers.ModelSerializer):
     extra_kwargs = {
       'password' : {'write_only' : True} # 사용자 정보를 불러올 떄 비밀번호는 빼고 가저온다.
     }
+
+  def create(self, validatad_data):
+    password = validatad_data.pop('password', None)
+    instance = self.Meta.model(**validatad_data)
+    if password is not None:
+      instance.set_password(password)
+    instance.save()
+    return instance
